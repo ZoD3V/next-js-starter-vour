0d9995122ed2c3db158cf5479f7406c0a54192b5
ZoD3V <adzkiyadzikrullah@gmail.com>
2026-01-14 12:43:00 +0700

    add custom theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { Button } from '@/components/ui/button';
+
 const Page = () => {
-    return <div>Home</div>;
+    return (
+        <div className='flex h-full w-full items-center justify-center'>
+            <Button>Button</Button>
+        </div>
+    );
 };
 
 export default Page;
